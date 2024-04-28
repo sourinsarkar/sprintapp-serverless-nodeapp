ca2eb8d825e1b5a0bb5674a2e6d34159e4f5de0f
Sourin <iamsourinsarkar@gmail.com>
2024-04-28 14:37:49 +0530

    Implemented: Serverless Dotenv Plugin

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
+    DEBUG: process.env.DEBUG === 1 || process.env.DEBUG === `1`,
     DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL: "not here",
   });
 });
